f4c0416a968f8506dde0e39763557a255b564937
krishnendu Dakshi <kdakshi2020@gamil.com>
2022-11-26 21:08:49 +0530

    added protected route logic

--- src/utils/ProtectedRoutes.tsx
+++ src/utils/ProtectedRoutes.tsx
@@ -1,8 +1,41 @@
-import React from 'react'
-import { redirect } from 'react-router-dom'
-export const ProtectedRoutes:React.FC<{children: JSX.Element}> = ({children}) => {
+import React,{useState,useEffect} from "react";
+import { redirect, useNavigate } from "react-router-dom";
+import { HOST } from "../../config/default";
 
-    // if user is not logged in redirect him to login page
-
-    return children;
+interface Response{
+    statusCode : number;
 }
+export const ProtectedRoutes: React.FC<{ children: JSX.Element }> = ({
+  children,
+}) => {
+    const [response, setResponse] = useState<Response>();
+    const navigate = useNavigate();
+    const token = localStorage.getItem("authToken");
+  // if user is not logged in redirect him to login page
+  useEffect(()=>{
+    if (token) {
+      fetch(`${HOST}/api/v1/user/verifyToken`, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: "Bearer ".concat(token),
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setResponse(data);
+        });
+    }
+    else{
+        navigate('/login');
+    }
+  },[token])
+  console.log(response);
+  if(response?.statusCode===200){
+    return children;
+  }
+  else{
+    return <></>
+  }
+};
